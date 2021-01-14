12c7cd394f068c2e10e1d2ffeeb0b73291205257
zhang-creator <zh194190070@163.com>
2021-01-14 22:16:49 +0800

    test-1

--- static/data.js
+++ static/data.js
@@ -1,7 +1,8 @@
 const VIDEOS = [
   {
     id: 0,
-    videoUrl: "http://qjzbs7gus.hn-bkt.clouddn.com/111.mp4",
+    // videoUrl: "http://qjzbs7gus.hn-bkt.clouddn.com/111.mp4",
+    videoUrl: "https://v.sogou.com/vc/np?type=play&link=mvsdezlcgy4dayjvmy3wkzrqgu3geztbmzrdizrxmmydan3egy3qsmjwgeydmmztgq3diclon5zg2ylml5ygyylzbfuhi5dqom5c6l3xo53s42lynftxkyjomnxw2l3jgy3teojugq2tknrvgizdgojshaztenzph5the33nozzw6z3pou6tcjtvorwv643povzggzj5onxwo33vl5shkyloonugs4djnythk5dnl5wwkzdjovwt243pm5xxkx3smvtgk4tsmfwcm5lunvpwgylnobqwsz3ohvrw633qmvzgc5djn5xa&query=%E6%8A%96%E9%9F%B3&docid=fd0cea76be18023c-49d895bce49191af-6cf12b203d518b29f1ca6caebc807914",
     poster: "https://image.pearvideo.com/cont/20201104/cont-1705200-12501405.jpg",
     likenum: 520,
     author: "../static/image/a1.png",
